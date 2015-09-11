e4056b9f3c75c1e6661c7524bccfd667b2f3fa25
ayanamimcy <mcy1208203968@hotmail.com>
2015-09-11 17:58:20 +0800

    some changes

---
+++ menu.lua
@@ -0,0 +1,69 @@
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Avahi SSH服务器浏览器", "/usr/bin/bssh", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Avahi VNC服务器浏览器", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"BCloud", "bcloud-gui", "/usr/share/icons/hicolor/16x16/apps/bcloud.png" },
+     {"Firefox", "firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"Google Chrome", "/usr/bin/google-chrome-stable ", "/usr/share/icons/hicolor/16x16/apps/google-chrome.png" },
+     {"XChat IRC", "xchat", "/usr/share/icons/hicolor/24x24/apps/xchat.png" },
+     {"Zenmap", "zenmap ", "///usr/share/zenmap/pixmaps/zenmap.png" },
+     {"Zenmap (as root)", "/usr/share/zenmap/su-to-zenmap.sh ", "///usr/share/zenmap/pixmaps/zenmap.png" },
+     {"qBittorrent", "qbittorrent ", "/usr/share/icons/hicolor/16x16/apps/qbittorrent.png" },
+ }
+
+ local menudb53804b7d726967c58fcc8c9ca03d27 = {
+     {"文档查看器", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+ }
+
+ local menue1b7cee794666669f55acead93513971 = {
+     {"Feh", "feh ", "///usr/share/feh/images/feh.png" },
+     {"gThumb", "gthumb ", "/usr/share/icons/hicolor/16x16/apps/gthumb.png" },
+     {"文档查看器", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+ }
+
+ local menu526fe18cc54d217db3fdb0bf6c5e9bc8 = {
+     {"Audacious", "audacious ", "/usr/share/icons/hicolor/48x48/apps/audacious.png" },
+     {"GNOME MPlayer", "gnome-mplayer ", "/usr/share/icons/hicolor/16x16/apps/gnome-mplayer.png" },
+ }
+
+ local menude71492af019157e5a5ef958b570e760 = {
+     {"Avahi Zero浏览器", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Fcitx", "fcitx", "/usr/share/icons/hicolor/16x16/apps/fcitx.png" },
+     {"GParted", "/usr/bin/gparted_polkit ", "/usr/share/icons/hicolor/16x16/apps/gparted.png" },
+     {"Guake Terminal", "guake", "/usr/share/icons/hicolor/16x16/apps/guake.png" },
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"LX 终端", "lxterminal", "/usr/share/icons/hicolor/128x128/apps/lxterminal.png" },
+     {"Oracle VM VirtualBox", "VirtualBox ", "/usr/share/icons/hicolor/16x16/mimetypes/virtualbox.png" },
+     {"Thunar 文件管理器", "thunar ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
+     {"Xfce 终端", "xfce4-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"dconf 系统配置编辑器", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/dconf-editor.png" },
+     {"任务管理器", "xfce4-taskmanager", "/usr/share/icons/gnome/16x16/apps/utilities-system-monitor.png" },
+     {"批量重命名", "/usr/lib/Thunar/ThunarBulkRename ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
+ }
+
+ local menu41282bf95e43c64d579757573a03cdde = {
+     {"CMake", "cmake-gui ", "/usr/share/icons/hicolor/32x32/apps/CMakeSetup.png" },
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
+     {"ipython", "xterm -e ipython"},
+ }
+
+ local menuc9a6ee90f5d43732e3f6cf4dcaa8493c = {
+     {"Mousepad", "mousepad ", "/usr/share/icons/gnome/16x16/apps/accessories-text-editor.png" },
+     {"Redshift", "redshift-gtk"},
+     {"ipython", "xterm -e ipython"},
+     {"归档管理器", "file-roller ", "/usr/share/icons/hicolor/16x16/apps/file-roller.png" },
+     {"截图", "xfce4-screenshooter", "/usr/share/icons/hicolor/48x48/apps/applets-screenshooter.png" },
+ }
+
+xdgmenu = {
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"办公", menudb53804b7d726967c58fcc8c9ca03d27},
+    {"图像", menue1b7cee794666669f55acead93513971},
+    {"影音", menu526fe18cc54d217db3fdb0bf6c5e9bc8},
+    {"系统工具", menude71492af019157e5a5ef958b570e760},
+    {"编程", menu41282bf95e43c64d579757573a03cdde},
+    {"附件", menuc9a6ee90f5d43732e3f6cf4dcaa8493c},
+}
+
